41545007c0cabb38878f997f201455a5993f4572
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-24 21:01:53 -0500

    Configure a SSH server with Puppet

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Configure the SSH server to authenticates only through SSH keys
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'BatchMode yes',
+  match  => '^BatchMode'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => '^IdentityFile'
+}
