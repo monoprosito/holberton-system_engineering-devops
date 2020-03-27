0a132f1507958dc9d5dd880a9ff3dd47701d024a
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-27 01:21:14 -0500

    Install Nginx web server with Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,42 @@
+# Install Nginx web server with Puppet
+include stdlib
+
+$link = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+$content = "\trewrite ^/redirect_me/$ ${link} permanent;"
+
+exec { 'update packages':
+  command => '/usr/bin/apt-get update'
+}
+
+exec { 'allow port 80':
+  command => "/usr/sbin/ufw allow 'Nginx HTTP'",
+  require => Package['nginx']
+}
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Package['nginx']
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  notify  => Exec['allow port 80'],
+  require => Exec['update packages']
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Holberton School',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root'
+}
+
+file_line { 'Set 301 redirection':
+  ensure   => 'present',
+  after    => 'server_name\ _;',
+  path     => '/etc/nginx/sites-available/default',
+  multiple => true,
+  line     => $content,
+  notify   => Exec['restart nginx']
+}
